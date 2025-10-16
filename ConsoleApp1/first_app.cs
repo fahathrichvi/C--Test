d63ff2ad838d7fa45a8c3c7967ce5eb379f1ee03
fahathrichvi <fahathrichvi7@gmail.com>
2025-10-16 22:03:35 +0300

    C# FirstAPP

---
+++ ConsoleApp1/first_app.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class first_app
+{
+    public static void Main(String[]args)
+    {
+        Console.WriteLine("Hello world");
+
+
+        Console.WriteLine("Enter Your Name: ");
+        String name = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Your Name is: " + name);
+
+        Console.WriteLine("End Precess");
+
+    }
+}
