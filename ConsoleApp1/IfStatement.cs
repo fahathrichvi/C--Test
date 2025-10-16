65d46547f389a2ae95de6102a799cadd52488810
fahathrichvi <fahathrichvi7@gmail.com>
2025-10-16 22:30:23 +0300

    add if statement

---
+++ ConsoleApp1/IfStatement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class IfStatement
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You Adult");
+
+            }
+            else
+            {
+                Console.WriteLine("You Child");
+
+            }
+        }
+
+    }
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d00300cc52c97a334a95f8d2a67c50b7f4a86336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0c16391724db048aed651c1eedc8182eb4a36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
